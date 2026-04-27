3bcc3450dc9298338038da3d48b3b3e94dc74c91
iitrsquad <iitrsquad@gmail.com>
2026-04-27 18:19:12 +0530

    Updated package-lock.json

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,6 +3,7 @@ import { useAuth } from './contexts/AuthContext';
 import PublicSite from './PublicSite';
 import PropertyPage from './pages/PropertyPage';
 import CityListingPage from './pages/CityListingPage';
+import RishikeshStaysPage from './pages/RishikeshStaysPage';
 import NewHomepage from './components/NewHomepage';
 import AuthRouter from './pages/auth/AuthRouter';
 import HostDashboardLayout from './pages/host/HostDashboardLayout';
@@ -82,6 +83,9 @@ export default function AppRouter() {
 
     if (currentPath.startsWith('/stays/')) {
       const citySlug = currentPath.split('/stays/')[1].split('/')[0];
+      if (citySlug === 'rishikesh') {
+        return <RishikeshStaysPage />;
+      }
       return <CityListingPage city={citySlug} />;
     }
 
